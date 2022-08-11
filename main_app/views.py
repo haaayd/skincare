cdfda77199328cf5470edcbdd94a1adec5b8ea92
haayd <haydee.sramirez@gmail.com>
2022-08-10 20:35:01 -0700

    display profile user in about page

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,5 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
     return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+def about(request):
+    return HttpResponse('<h1>About the Profile-user</h1>')
